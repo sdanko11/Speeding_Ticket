88b0767c24e799ad260fce6b4e7e17c3e036a529
steve <stevedanko1@gmail.com>
2014-02-14 13:33:36 -0500

    refactored

--- speeding_ticket.rb
+++ speeding_ticket.rb
@@ -1,17 +1,12 @@
 def ticket(my_speed, birthday)
+  if birthday == true
+    my_speed = (my_speed - 6)
+  end
   if my_speed <= 70
     return 0
   elsif my_speed <= 90 && my_speed >= 71
-    if birthday == true && my_speed <= 77
-      return 0
-    else 
-      return 1
-    end
+    return 1
   elsif my_speed >= 91 
-    if birthday == true && my_speed <= 97
-      return 1
-    else 
-      return 2
-    end
+    return 2
   end
 end
